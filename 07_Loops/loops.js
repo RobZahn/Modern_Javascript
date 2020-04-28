852e2c2168c50698a7517b1bda1a59dad1e8deb5
Robert Zahn <rob.jzahn@gmail.com>
2020-04-28 01:33:31 -0400

    Update loops notes

---
+++ 07_Loops/loops.js
@@ -0,0 +1,13 @@
+// INTRO TO LOOPS
+
+// Loops are concise methods for repeating code
+
+// Multiple types:
+// for loop
+// while loop
+// for...of loop
+// for...in loop
+
+// _______________________________________________________________________
+
+// FOR LOOPS
