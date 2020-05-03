1834a9ceb993967ac2f6787884db494c1762f76b
Robert Zahn <rob.jzahn@gmail.com>
2020-05-03 14:17:12 -0400

    Update functions notes

--- 08_Functions/01_firstFunction.js
+++ 08_Functions/01_firstFunction.js
@@ -24,3 +24,56 @@ function throwDice(numRolls) {
 function greet(name) {
   console.log(`Hi, ${name}!`);
 }
+
+// Squares a number
+
+function square(num) {
+  console.log(num * num);
+}
+
+// Finds the sum of two numbers
+
+function sum(x, y) {
+  console.log(x + y);
+}
+
+// Divides two numbers
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+// RETURN
+
+// RETURN statements end function execution immediately and return a single value.
+
+function returnSum(x, y) {
+  return x + y;
+}
+
+// Checks if a color is purple
+
+function isPurple(color) {
+  if (color.toLowerCase() === "purple") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Refactored version
+
+function isPurpleRefactored(color) {
+  return color.toLowerCase() === "purple";
+}
+
+// Loops through an array and returns true if a color is purple or magenta and false if neither exists.
+
+function containsPurple(colorsArr) {
+  for (let color of colorsArr) {
+    if (color === "purple" || color === "magenta") {
+      return true;
+    }
+  }
+  return false;
+}
