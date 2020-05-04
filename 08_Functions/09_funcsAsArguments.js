33b1dd71957fa8eddf8337c11d35b2181b34b73f
Robert Zahn <rob.jzahn@gmail.com>
2020-05-04 04:37:58 -0400

    Update functions notes

--- 08_Functions/09_funcsAsArguments.js
+++ 08_Functions/09_funcsAsArguments.js
@@ -13,3 +13,37 @@ callTwice(laugh);
 
 // Hahaha!
 // Hahaha!
+
+//___________________________________________________________________________
+
+function repeatNTimes(action, num) {
+  for (let i = 0; i < num; i++) {
+    action();
+  }
+}
+
+function rage() {
+  console.log("I HATE EVERYTHING!");
+}
+
+repeatNTimes(rage, 5); // 'I HATE EVERYTHING' x 5
+
+//___________________________________________________________________________
+
+// Randomly call one of two functions:
+
+function cry() {
+  console.log("I'm so sad!");
+}
+
+function randomlyPick(func1, func2) {
+  let rand = Math.random();
+
+  if (rand < 0.5) {
+    func1();
+  } else {
+    func2();
+  }
+}
+
+randomlyPick(cry, rage);
