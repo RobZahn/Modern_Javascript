9e75edd17fcdb56ed74729913aca437802a3564d
Robert Zahn <rob.jzahn@gmail.com>
2020-05-04 03:46:02 -0400

    Update functions notes

--- 08_Functions/06_functionScope.js
+++ 08_Functions/06_functionScope.js
@@ -10,6 +10,8 @@ function helpMe() {
 
 // msg; // NOT DEFINED
 
+//___________________________________________________________________________
+
 // BLOCK SCOPE
 
 // LET and CONST are scoped to the block they are defined in. VAR is NOT block scoped - it is only scoped to Functions. Nowadays, it is bad practice to use VAR.
@@ -27,3 +29,34 @@ if (true) {
 }
 
 console.log(animal); // 'eel'
+
+//___________________________________________________________________________
+
+// LEXICAL SCOPE
+
+// Nested functions are lexically bound to the scope of their parent functions. This is a one way relationship: variables defined within the inner function are not available in the outer parent function.
+
+// In the below example, we have access to hero within the nested function.
+
+function outer() {
+  let hero = "Black Panther";
+
+  function inner() {
+    let cryForHelp = `${hero}, please save me!`;
+    console.log(cryForHelp);
+  }
+
+  inner();
+}
+
+// Note that it is impossible to call innerTwo from OUTSIDE of the parent function because the inner function is scoped to the parent function.
+
+function outerTwo() {
+  let movie = "Amadeus";
+
+  function innerTwo() {
+    console.log(movie.toUpperCase());
+  }
+
+  innerTwo();
+}
