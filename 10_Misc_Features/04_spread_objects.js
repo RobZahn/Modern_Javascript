556aeefd8423f51ee22abf0181e61560d0e95697
Robert Zahn <rob.jzahn@gmail.com>
2020-05-06 08:36:06 -0400

    Update misc features notes

--- 10_Misc_Features/04_spread_objects.js
+++ 10_Misc_Features/04_spread_objects.js
@@ -22,3 +22,17 @@ const catDog = { ...feline, ...canine };
 // { legs: 4, family: 'Caninae', furry: true }
 
 // NOTE: Duplicate conflicting key-value pairs will be overwritten. Order matters. In the catDog example, family: Caninae was the last family value and so it overwrote the feline family value.
+
+//______________________________________________________________________________
+
+// We can make copies of object literals just like with strings:
+
+const catDogClone = { ...catDog };
+// { legs: 4, family: 'Caninae', furry: true }
+
+// NOTE: It is only possible to spread an object to another object. We can however spread an array into an object:
+
+{...[4,5,6]}
+// {0: 4, 1: 5, 2: 6}
+
+// The keys will automatically populate themselves based on the indices of the elements of the array.
