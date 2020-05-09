9306ba4977ca9fd063ccefc1ed2832d7e23c84e2
Robert Zahn <rob.jzahn@gmail.com>
2020-05-09 09:30:04 -0400

    Update DOM manipulation notes

---
+++ 12_DOM_Manipulation/01_DOM_intro.js
@@ -0,0 +1,21 @@
+// DOM Intro
+
+// The Document object is the entry point into the DOM. It contains representations of all the content on a page, plus tons of useful methods and properties. Objects within the document are assembled into a tree.
+
+// Ex.
+
+<body>
+  <h1>Hello!</h1>
+  <ul>
+    <li>Water Plants</li>
+    <li>Get Some Sleep</li>
+  </ul>
+</body>;
+
+//                              [DOCUMENT] --> Root
+//                                  |
+//                                BODY
+//                               /   \
+//                             H1     UL
+//                                   / \
+//                                 LI  LI
