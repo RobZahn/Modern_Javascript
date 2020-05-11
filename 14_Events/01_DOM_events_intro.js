792f2f55d04913fceda872b808f9ef5b344db546
Robert Zahn <rob.jzahn@gmail.com>
2020-05-11 14:13:15 -0400

    Update DOM notes

---
+++ 14_Events/01_DOM_events_intro.js
@@ -0,0 +1,9 @@
+// Events pattern:
+
+// Target Element      -       Event Type          -     Code to run
+//
+// Button              -       Click               -     Change color
+//
+// Input               -       Return/Enter key    -     Get search results
+//
+// Image               -       Mouseover           -     Display image caption
