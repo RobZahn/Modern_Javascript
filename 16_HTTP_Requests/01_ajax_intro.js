98cb44268a243cffc06836236747d331a74dbfbb
Robert Zahn <rob.jzahn@gmail.com>
2020-05-16 12:44:38 -0400

    Update HTTP notes

---
+++ 16_HTTP_Requests/01_ajax_intro.js
@@ -0,0 +1,30 @@
+// **************************************
+// AJAX - Asynchronous JavaScript and XML
+// **************************************
+
+// AJAX is an approach to getting data and potentially changing the contents
+// of a webpage without needing to reload the current page or redirect to
+// a new page.
+
+// Although X in Ajax stands for XML, JSON is used more than XML nowadays
+// because of its many advantages such as being lighter and a part of
+// JavaScript. Both JSON and XML are used for packaging information in the
+// Ajax model.
+
+// ********************************
+// XML - Extensible Markup Language
+// ********************************
+
+// Uses tags like HTML; they do not represent elements but rather keys that
+// are associated with the values inside of them.
+
+// name -> todd
+// email -> todd@gmail
+// age -> 40
+
+<name>
+  <first>Todd</first>
+  <last>Jones</last>
+</name>
+<email>todd@gmail</email>
+<age>40</age>
