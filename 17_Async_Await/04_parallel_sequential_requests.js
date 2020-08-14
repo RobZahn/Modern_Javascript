e0230d78a961a7d1da9237b3389908fe911f5148
Robert Zahn <rob.jzahn@gmail.com>
2020-08-14 19:28:22 -0400

    Add await parallel/sequential notes

---
+++ 17_Async_Await/04_parallel_sequential_requests.js
@@ -0,0 +1,64 @@
+// ********************************
+// Parallel vs. Sequential Requests
+// ********************************
+
+// Sequential:
+
+// async function get3Pokemon() {
+//   // these requests are happening in sequence. the first has to finish before the next will run.
+//   const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//   const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//   const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// // Parallel:
+
+async function get3Pokemon() {
+  // these requests are happening in parallel. In this case, prom1, prom2, prom3 contain a promise and NOT the value of the resolved promise.
+  const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+  const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+  const prom3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+
+  const poke1 = await prom1;
+  const poke2 = await prom2;
+  const poke3 = await prom3;
+
+  console.log(poke1.data);
+  console.log(poke2.data);
+  console.log(poke3.data);
+}
+
+get3Pokemon();
+
+function changeBodyColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+}
+
+async function lightShowSequential() {
+  await changeBodyColor('teal', 1000);
+  await changeBodyColor('pink', 1000);
+  await changeBodyColor('indigo', 1000);
+  await changeBodyColor('green', 1000);
+}
+
+async function lightShowParallel() {
+  const p1 = changeBodyColor('teal', 1000);
+  const p2 = changeBodyColor('pink', 1000);
+  const p3 = changeBodyColor('indigo', 1000);
+  const p4 = changeBodyColor('green', 1000);
+  await p1;
+  await p2;
+  await p3;
+  await p4;
+}
+
+// lightShowSequential();
+lightShowParallel();
