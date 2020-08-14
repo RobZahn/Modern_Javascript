27de296274388cd4d718524deb63a068fc56c3b8
Robert Zahn <rob.jzahn@gmail.com>
2020-08-14 04:18:07 -0400

    Add refactor fetch chains notes

--- 16_HTTP_Requests/03_fetch.js
+++ 16_HTTP_Requests/03_fetch.js
@@ -12,21 +12,24 @@
 
 // Basic Syntax:
 
-// fetch('https://swapi.dev/api/planets/')
-//   .then(response => {
-//     if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+fetch('https://swapi.dev/api/planets/')
+  .then(response => {
+    if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
 
-//     return response.json();
-//   })
-//   .then(planets => {
-//     for (let planet of planets.results) {
-//       console.log(planet.name);
-//     }
-//   })
-//   .catch(err => {
-//     console.log('Something went wrong.');
-//     console.log(err);
-//   });
+    return response.json();
+  })
+  .then(planets => {
+    for (let planet of planets.results) {
+      console.log(planet.name);
+    }
+  })
+  .then(() => {
+    console.log('-----');
+  })
+  .catch(err => {
+    console.log('Something went wrong.');
+    console.log(err);
+  });
 
 // ___________________________________________________________________________
 
@@ -51,9 +54,45 @@ fetch('https://swapi.dev/api/planets/')
   .then(movieData => {
     console.log(`Fetched first film - ${movieData.title}`);
   })
+  .then(() => {
+    console.log('-----');
+  })
   .catch(err => {
     console.log('Something went wrong.');
     console.log(err);
   });
 
 // ___________________________________________________________________________
+
+// Refactoring Fetch chains:
+
+const checkStatusAndParse = response => {
+  if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+
+  return response.json();
+};
+
+const printPlanets = planets => {
+  console.log('--------------------');
+  console.log('Loaded 10 planets...');
+  console.log('--------------------');
+  for (let planet of planets.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(planets.next); // Creates new resolved promise to pass on to another .then()
+};
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+  return fetch(url);
+};
+
+fetchNextPlanets()
+  .then(checkStatusAndParse)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(checkStatusAndParse)
+  .then(printPlanets)
+  .catch(err => {
+    console.log('Something went wrong.');
+    console.log(err);
+  });
