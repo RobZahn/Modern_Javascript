546b2fe304cd10eab7604c0ff2ef63e1f9bb9d4d
Robert Zahn <rob.jzahn@gmail.com>
2020-08-14 17:27:04 -0400

    Add error handling in async funcs notes

--- 17_Async_Await/02_await_keyword.js
+++ 17_Async_Await/02_await_keyword.js
@@ -29,3 +29,25 @@ async function getPlanetsAwait() {
 }
 
 getPlanetsAwait();
+
+// _____________________________________________________________________________
+
+// Error Handling in Async Funcs:
+
+async function getPlanetsAwait1() {
+  try {
+    const res = await axios.get('https://swapi.dev/api/planetssss/');
+    console.log(res.data);
+  } catch (error) {
+    console.log('In Catch!', error);
+  }
+}
+
+getPlanetsAwait1();
+
+// In case of 404 error:
+
+// In Catch! Error: Request failed with status code 404
+// at e.exports (spread.js:25)
+// at e.exports (spread.js:25)
+// at XMLHttpRequest.l.onreadystatechange (spread.js:25)
