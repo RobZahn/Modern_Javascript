1b71b4714165b1d5a9b5f3afe9200b2453146ba6
Robert Zahn <rob.jzahn@gmail.com>
2020-08-14 04:50:41 -0400

    Add refactor axios notes

--- 16_HTTP_Requests/04_axios.js
+++ 16_HTTP_Requests/04_axios.js
@@ -7,10 +7,34 @@
 // Bad status codes will be caught automatically.
 
 axios
-  .get('https://swapi.dev/api/planetsy/')
+  .get('https://swapi.dev/api/planets/')
   .then(res => {
     console.log(res.data);
   })
+  .catch(err => {
+    console.log(err);
+  });
+
+// ___________________________________________________________________________
+
+// Sequential requests:
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+  return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+  console.log(data);
+  for (let planet of data.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(data.next);
+};
+
+fetchNextPlanets()
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
   .catch(err => {
     console.log(err);
   });
