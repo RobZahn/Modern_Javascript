d9e5fe4470044dfebb78870569e7d4e8295df48a
Robert Zahn <rob.jzahn@gmail.com>
2020-08-17 10:07:34 -0400

    Timer project setup

---
+++ 20_Drawing_Animations/index.js
@@ -0,0 +1,15 @@
+class Timer {
+  constructor(durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput;
+    this.startBtn = startBtn;
+    this.pauseBtn = pauseBtn;
+  }
+
+  start() {}
+
+  pause() {}
+
+  onDurationChange() {}
+
+  tick() {}
+}
