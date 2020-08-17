2ecb401187668ad76f20fde59e83ad29f8c617d5
Robert Zahn <rob.jzahn@gmail.com>
2020-08-17 10:17:37 -0400

    Add query selectors

--- 20_Drawing_Animations/index.js
+++ 20_Drawing_Animations/index.js
@@ -1,11 +1,22 @@
+// Query Selectors
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start-btn');
+const pauseBtn = document.querySelector('#pause-btn');
+
+// Timer Class
+
 class Timer {
   constructor(durationInput, startBtn, pauseBtn) {
     this.durationInput = durationInput;
     this.startBtn = startBtn;
     this.pauseBtn = pauseBtn;
+
+    this.startBtn.addEventListener('click', this.start());
   }
 
-  start() {}
+  start() {
+    console.log('Timer starting...');
+  }
 
   pause() {}
 
@@ -13,3 +24,5 @@ class Timer {
 
   tick() {}
 }
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
