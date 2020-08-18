307fbd567e099b81be807b2ebe6de33561ed67dd
Robert Zahn <rob.jzahn@gmail.com>
2020-08-18 06:44:59 -0400

    Make follow up request with selected movie

--- 21_Application_Design_Patterns/index.js
+++ 21_Application_Design_Patterns/index.js
@@ -15,7 +15,7 @@ const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
-// Fetch Movies
+// Fetch Data helper
 const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
@@ -29,7 +29,7 @@ const fetchData = async searchTerm => {
   return response.data.Search;
 };
 
-// Populate dropdown with requested movies
+// Build dropdown on input
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
   if (!movies.length) {
@@ -40,6 +40,38 @@ const onInput = async event => {
   resultsWrapper.innerHTML = '';
   activateDropdown();
   populateDropdown(movies);
+};
+
+// Populate dropdown
+const populateDropdown = movies => {
+  for (let movie of movies) {
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${imgSrc}">
+      ${movie.Title}
+      `;
+
+    option.addEventListener('click', () => {
+      input.value = movie.Title;
+      deactivateDropdown();
+      onMovieSelect(movie);
+    });
+
+    resultsWrapper.append(option);
+  }
+};
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'e1d3aa51',
+      i: movie.imdbID
+    }
+  });
+  console.log(response.data);
 };
 
 // Event Handlers
--- 21_Application_Design_Patterns/utils.js
+++ 21_Application_Design_Patterns/utils.js
@@ -17,26 +17,5 @@ const debounce = (func, delay = 1000) => {
   };
 };
 
-// Populate dropdown
-const populateDropdown = movies => {
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${imgSrc}">
-      ${movie.Title}
-      `;
-
-    option.addEventListener('click', () => {
-      input.value = movie.Title;
-      deactivateDropdown();
-    });
-
-    resultsWrapper.append(option);
-  }
-};
-
 const activateDropdown = () => dropdown.classList.add('is-active');
 const deactivateDropdown = () => dropdown.classList.remove('is-active');
