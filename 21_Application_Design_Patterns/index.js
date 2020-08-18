c09f10f6b14634ad70628798cc8c98fa2cdd2835
Robert Zahn <rob.jzahn@gmail.com>
2020-08-18 05:05:32 -0400

    Handle broken imgs

--- 21_Application_Design_Patterns/index.js
+++ 21_Application_Design_Patterns/index.js
@@ -38,12 +38,13 @@ const onInput = async event => {
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
-    option.classList.add('dropdown-item');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
+    option.classList.add('dropdown-item');
     option.innerHTML = `
-    <img src="${movie.Poster}">
-    ${movie.Title}
-    `;
+      <img src="${imgSrc}">
+      ${movie.Title}
+      `;
 
     resultsWrapper.append(option);
   }
