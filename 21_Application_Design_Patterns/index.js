8bf037f7fb14531a483b0c316181ecd1ebebdf95
Robert Zahn <rob.jzahn@gmail.com>
2020-08-19 12:21:44 -0400

    Clean up specific references

--- 21_Application_Design_Patterns/index.js
+++ 21_Application_Design_Patterns/index.js
@@ -2,19 +2,6 @@
 // Index
 // *****
 
-const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'd9835cc5',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) return [];
-
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector('.autocomplete-1'),
 
@@ -32,6 +19,19 @@ createAutoComplete({
 
   inputValue(movie) {
     return movie.Title;
+  },
+
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) return [];
+
+    return response.data.Search;
   }
 });
 
