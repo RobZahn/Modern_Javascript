59ea128e7b6c1defc5575d86a13ae15aef7fed1c
Robert Zahn <rob.jzahn@gmail.com>
2020-08-19 11:33:58 -0400

    Store autocomplete logic in discrete function

--- 21_Application_Design_Patterns/index.js
+++ 21_Application_Design_Patterns/index.js
@@ -1,100 +1,59 @@
-// Config widget HTML
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><strong>Search for a Movie</strong></label>
-<input type="text" class="input" />
-<div class="dropdown">
-<div class="dropdown-menu">
-<div class="dropdown-content results"></div>
-</div>
-</div>
-`;
-
-// Query Selectors
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-const summary = document.querySelector('#summary');
-
-// Fetch Data helper
 const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'e1d3aa51',
+      apikey: 'd9835cc5',
       s: searchTerm
     }
   });
 
-  if (response.data.Error) return [];
+  if (response.data.Error) {
+    return [];
+  }
 
   return response.data.Search;
 };
 
-// Build dropdown on input
-const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-  if (!movies.length) {
-    deactivateDropdown();
-    return;
-  }
+createAutoComplete({
+  root: document.querySelector('.autocomplete-1')
+});
 
-  resultsWrapper.innerHTML = '';
-  activateDropdown();
-  populateDropdown(movies);
-};
+createAutoComplete({
+  root: document.querySelector('.autocomplete-2')
+});
 
-// Populate dropdown
-const populateDropdown = movies => {
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+createAutoComplete({
+  root: document.querySelector('.autocomplete-3')
+});
 
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${imgSrc}">
-      ${movie.Title}
-      `;
-
-    option.addEventListener('click', () => {
-      input.value = movie.Title;
-      deactivateDropdown();
-      onMovieSelect(movie);
-    });
-
-    resultsWrapper.append(option);
-  }
-};
-
-// Make follow up request with selected movie
 const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'e1d3aa51',
+      apikey: 'd9835cc5',
       i: movie.imdbID
     }
   });
 
-  summary.innerHTML = movieTemplate(response.data);
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetails => {
   return `
-  <article class="media">
-    <figure class="media-left">
-      <p class="image">
-        <img src="${movieDetails.Poster}" />
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <h1>${movieDetails.Title}</h1>
-        <h4>${movieDetails.Genre}</h4>
-        <p>${movieDetails.Plot}</p>
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetails.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h2>${movieDetails.Title}</h2>
+          <h4>${movieDetails.Year}</h4>
+          <h5>${movieDetails.Genre}</h5>
+          <p>${movieDetails.Plot}</p>
+        </div>
       </div>
-    </div>
-  </article>
-
-  <article class="notification is-primary">
+    </article>
+    <article class="notification is-primary">
       <p class="title">${movieDetails.Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
@@ -116,12 +75,3 @@ const movieTemplate = movieDetails => {
     </article>
   `;
 };
-
-// Event Handlers
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    deactivateDropdown();
-  }
-});
