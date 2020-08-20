8debcb65a02c383d05ea4dcb86021e554c9d2655
Robert Zahn <rob.jzahn@gmail.com>
2020-08-20 12:54:05 -0400

    Generate random shapes

--- 22_Maze/index.js
+++ 22_Maze/index.js
@@ -8,14 +8,18 @@ const {
   World
 } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 
@@ -30,12 +34,26 @@ World.add(
 
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 20, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 20, { isStatic: true }),
-  Bodies.rectangle(0, 300, 20, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 20, 600, { isStatic: true })
+  Bodies.rectangle(400, 0, 800, 5, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 5, { isStatic: true }),
+  Bodies.rectangle(0, 300, 5, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 5, 600, { isStatic: true })
 ];
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Generate random shapes
+
+for (let i = 0; i < 40; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35)
+    );
+  }
+}
