470d1cf356b0132a745509b3933ff03612a9e4ef
Robert Zahn <rob.jzahn@gmail.com>
2020-08-26 21:51:30 -0400

    Draw player ball

--- 22_Maze/index.js
+++ 22_Maze/index.js
@@ -134,6 +134,8 @@ verticals.forEach((row, rowIndex) => {
   });
 });
 
+// Goal
+
 const goal = Bodies.rectangle(
   width - unitLength / 2,
   height - unitLength / 2,
@@ -143,3 +145,8 @@ const goal = Bodies.rectangle(
 );
 
 World.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+World.add(world, ball);
