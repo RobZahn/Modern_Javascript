c071b1bd10319bfddbf02cff72024cb51fb1585d
Robert Zahn <rob.jzahn@gmail.com>
2020-08-28 16:50:32 -0400

    Implement solution #1 - callbacks

--- 24_Node_CLI_Tools/index.js
+++ 24_Node_CLI_Tools/index.js
@@ -4,8 +4,30 @@
 
 const fs = require('fs');
 
+// Solution 1 - Callbacks (not ideal)
+
 fs.readdir(process.cwd(), (err, files) => {
   if (err) console.log(err);
 
-  console.log(files);
+  const allStats = Array(files.length).fill(null);
+
+  for (let file of files) {
+    const idx = files.indexOf(file);
+
+    fs.lstat(file, (err, stats) => {
+      if (err) console.log(err);
+
+      allStats[idx] = stats;
+
+      const ready = allStats.every(stats => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, idx) => {
+          console.log(files[idx], stats.isFile());
+        });
+      }
+    });
+  }
 });
