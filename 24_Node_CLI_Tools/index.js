c9b35746512b73c1315443c0bb46e70455e2e7a6
Robert Zahn <rob.jzahn@gmail.com>
2020-08-28 17:31:56 -0400

    Implement logic using promises

--- 24_Node_CLI_Tools/index.js
+++ 24_Node_CLI_Tools/index.js
@@ -1,33 +1,77 @@
 #!/usr/bin/env node
 
-// The above line instructs the computer to allow node to execute the file.
+// ^ The above line instructs the computer to allow node to execute the file.
 
 const fs = require('fs');
 
+// **********************************
 // Solution 1 - Callbacks (not ideal)
+// **********************************
 
-fs.readdir(process.cwd(), (err, files) => {
+// fs.readdir(process.cwd(), (err, files) => {
+//   if (err) console.log(err);
+
+//   const allStats = Array(files.length).fill(null);
+
+//   for (let file of files) {
+//     const idx = files.indexOf(file);
+
+//     fs.lstat(file, (err, stats) => {
+//       if (err) console.log(err);
+
+//       allStats[idx] = stats;
+
+//       const ready = allStats.every(stats => {
+//         return stats;
+//       });
+
+//       if (ready) {
+//         allStats.forEach((stats, idx) => {
+//           console.log(files[idx], stats.isFile());
+//         });
+//       }
+//     });
+//   }
+// });
+
+// *********************************
+// Solution 2 - Promise (less ideal)
+// *********************************
+
+// const { lstat } = fs.promises;
+
+// fs.readdir(process.cwd(), async (err, filenames) => {
+//   if (err) console.log(err);
+
+//   for (let filename of filenames) {
+//     try {
+//       const stats = await lstat(filename);
+
+//       console.log(filename, stats.isFile());
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+// });
+
+// **************************************
+// Solution 3 - Promise.all (best option)
+// **************************************
+
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) console.log(err);
 
-  const allStats = Array(files.length).fill(null);
+  const statPromises = filenames.map(filename => {
+    return lstat(filename);
+  });
 
-  for (let file of files) {
-    const idx = files.indexOf(file);
+  const allStats = await Promise.all(statPromises);
 
-    fs.lstat(file, (err, stats) => {
-      if (err) console.log(err);
+  for (let stats of allStats) {
+    const idx = allStats.indexOf(stats);
 
-      allStats[idx] = stats;
-
-      const ready = allStats.every(stats => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, idx) => {
-          console.log(files[idx], stats.isFile());
-        });
-      }
-    });
+    console.log(filenames[idx], stats.isFile());
   }
 });
