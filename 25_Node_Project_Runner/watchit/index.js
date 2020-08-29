91a2b0a1ffe013980d7680710916bede12176b82
Robert Zahn <rob.jzahn@gmail.com>
2020-08-28 23:39:58 -0400

    Implement CLI handling with caporal

--- 25_Node_Project_Runner/watchit/index.js
+++ 25_Node_Project_Runner/watchit/index.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-  console.log("Starting user's program");
-}, 100);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'));
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`File "${name}" does not exist.`);
+    }
+    const start = debounce(() => {
+      console.log("Starting user's program");
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
