928d68acdaf7a46e7556fd28960e86f938ea3138
Robert Zahn <rob.jzahn@gmail.com>
2020-08-28 23:04:40 -0400

    Debounce add event

--- 25_Node_Project_Runner/watchit/index.js
+++ 25_Node_Project_Runner/watchit/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
-
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+
+const start = debounce(() => {
+  console.log("Starting user's program");
+}, 100);
 
 chokidar
   .watch('.')
-  .on('add', () => console.log('FILE ADDED'))
+  .on('add', start)
   .on('change', () => console.log('FILE CHANGED'))
   .on('unlink', () => console.log('FILE UNLINKED'));
