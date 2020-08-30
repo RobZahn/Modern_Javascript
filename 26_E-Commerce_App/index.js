746e8d5dfe1e35474416c785614503637a6e45ad
Robert Zahn <rob.jzahn@gmail.com>
2020-08-30 02:12:35 -0400

    Implement body parser

--- 26_E-Commerce_App/index.js
+++ 26_E-Commerce_App/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`
@@ -7,7 +10,7 @@ app.get('/', (req, res) => {
     <form method="POST">
       <input type="email" name="email" placeholder="email">
       <input type="password" name="password" placeholder="password">
-      <input type="password" name="passwordConfirmation " placeholder="confirm password">
+      <input type="password" name="passwordConfirmation" placeholder="confirm password">
       <button>Sign Up</button>
     </form>
   </div>
@@ -15,16 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  req.on('data', data => {
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+  console.log(req.body);
   res.send('Account created!');
 });
 
