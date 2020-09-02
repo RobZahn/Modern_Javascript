59a487a6649fbdfeb30b8428c998fcf4fc92bfd0
Robert Zahn <rob.jzahn@gmail.com>
2020-09-02 16:34:35 -0400

    Save user records to users.json

--- 26_E-Commerce_App/repositories/users.js
+++ 26_E-Commerce_App/repositories/users.js
@@ -8,12 +8,15 @@ class UsersRepository {
     }
 
     this.filename = filename;
+
     try {
       fs.accessSync(this.filename);
     } catch (err) {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  // Get All
 
   async getAll() {
     return JSON.parse(
@@ -22,10 +25,21 @@ class UsersRepository {
       })
     );
   }
+
+  // Create
+
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password' });
 
   const users = await repo.getAll();
 
